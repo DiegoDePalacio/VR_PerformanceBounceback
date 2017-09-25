b5ae844be1fb49c5e20be10d05205592e267a0e6
DiegoDePalacio <dpalacio@yahoo.com>
2017-09-25 07:07:52 +0200

    The balls rigidbodies are now assigned and cached in editor time to avoid using GetComponent in runtime.

--- Assets/PerformanceBounceback/Scripts/BallSpawner.cs
+++ Assets/PerformanceBounceback/Scripts/BallSpawner.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public class Ball
+{
+    public GameObject container = null;
+    public Rigidbody rigidBody = null;
+}
 
 public class BallSpawner : MonoBehaviour {
 
     public static BallSpawner current;
 
-    public List<GameObject> pooledBalls; //the object pool
+    // If the number of ball is high, the manual assignation of the balls should be replaced
+    // with the addition of another variable for the number of balls and a Context menu to
+    // spawn in Editor time all the desired balls, filling automatically at the same time the 
+    // values inside this List 
+    public List<Ball> pooledBalls; //the object pool
     public static int ballPoolNum = 0; //a number used to cycle through the pooled objects
 
     private float cooldown;
@@ -15,10 +27,10 @@ public class BallSpawner : MonoBehaviour {
     void Awake()
     {
         current = this; //makes it so the functions in ObjectPool can be accessed easily anywhere
-        foreach ( GameObject ball in pooledBalls ) { ball.SetActive( false ); }
+        foreach ( Ball ball in pooledBalls ) { ball.container.SetActive( false ); }
     }
 
-    public GameObject GetPooledBall()
+    public Ball GetPooledBall()
     {
         ballPoolNum = ( ballPoolNum + 1 ) % pooledBalls.Count;
         return pooledBalls[ballPoolNum];
@@ -37,11 +49,10 @@ public class BallSpawner : MonoBehaviour {
 
     void SpawnBall()
     {
-        GameObject selectedBall = BallSpawner.current.GetPooledBall();
-        selectedBall.transform.position = transform.position;
-        Rigidbody selectedRigidbody = selectedBall.GetComponent<Rigidbody>();
-        selectedRigidbody.velocity = Vector3.zero;
-        selectedRigidbody.angularVelocity = Vector3.zero;
-        selectedBall.SetActive(true);
+        Ball selectedBall = BallSpawner.current.GetPooledBall();
+        selectedBall.container.transform.position = transform.position;
+        selectedBall.rigidBody.velocity = Vector3.zero;
+        selectedBall.rigidBody.angularVelocity = Vector3.zero;
+        selectedBall.container.SetActive(true);
     }
 }
