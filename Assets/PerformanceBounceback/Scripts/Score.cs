d9f62a6f96143f29536b3f8d71008518382bf174
DiegoDePalacio <dpalacio@yahoo.com>
2017-09-25 07:22:06 +0200

    The text of the Score component is assigned in editor time instead of using GetComponent each frame. An assertion was added in order to quckly spot when the text is not being assigned.

--- Assets/PerformanceBounceback/Scripts/Score.cs
+++ Assets/PerformanceBounceback/Scripts/Score.cs
@@ -8,16 +8,19 @@ public class Score : MonoBehaviour
 {
     public GameManager gameManager = null;
 
+    // Replace this for an array if more than one text should be handled by this component
+    public Text text = null;
+
 	// Use this for initialization
 	void Start ()
     {
         Assert.IsNotNull( gameManager, "GameManager is not yet assigned!" );
+        Assert.IsNotNull( text, "The Score text is not yet assigned!" );
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Text text = GetComponentInChildren<Text>();
         text.text = "Score: " + gameManager.score.ToString();
 	}
 }
