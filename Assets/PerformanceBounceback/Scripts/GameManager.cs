d70404eb29e96166c6fddfa78728fb8611bf334b
DiegoDePalacio <dpalacio@yahoo.com>
2017-09-25 07:09:45 +0200

    Removed empty Monobehaviour methods to improve performance.

--- Assets/PerformanceBounceback/Scripts/GameManager.cs
+++ Assets/PerformanceBounceback/Scripts/GameManager.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-
+public class GameManager : MonoBehaviour
+{
     public int score;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
