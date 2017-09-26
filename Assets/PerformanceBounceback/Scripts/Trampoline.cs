b22d71f29c551a73d0599dc146c8b7bd421454c3
DiegoDePalacio <dpalacio@yahoo.com>
2017-09-26 03:44:17 +0200

    Added the notification to the Editor for saving the assignation by code of the score script on the trampoline instances.

--- Assets/PerformanceBounceback/Scripts/Trampoline.cs
+++ Assets/PerformanceBounceback/Scripts/Trampoline.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class Trampoline : MonoBehaviour {
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
+public class Trampoline : MonoBehaviour
+{
     public ParticleSystem pSystem = null;
     public GameManager scoreScript = null;
 
+#if UNITY_EDITOR
     [ContextMenu( "Assign the score script" )]
     private void AssignTheScoreScript()
     {
         scoreScript = FindObjectOfType<GameManager>();
+        EditorUtility.SetDirty( this );
     }
+#endif
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         Assert.IsNotNull( scoreScript, "The score script in a Trampoline instance is not yet assigned!" );
         Assert.IsNotNull( pSystem, "The particle system in a Trampoline instance is not yet assigned!" );
