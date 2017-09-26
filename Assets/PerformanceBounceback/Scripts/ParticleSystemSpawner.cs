5f5ee0d7e8f1712c5a72fb9bac2cbfd4ad901c8f
DiegoDePalacio <dpalacio@yahoo.com>
2017-09-26 06:26:03 +0200

    Created a particle system pool.

---
+++ Assets/PerformanceBounceback/Scripts/ParticleSystemSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemSpawner : MonoBehaviour
+{
+    public static ParticleSystemSpawner current;
+
+    public List<ParticleSystem> pooledParticles;
+    public static int psPoolNum = 0;
+
+	void Awake ()
+    {
+        current = this;
+	}
+
+    private ParticleSystem GetPooledParticleSystem()
+    {
+        psPoolNum = ( psPoolNum + 1 ) % pooledParticles.Count;
+        return pooledParticles[psPoolNum];
+    }
+
+	public void SpawnParticleSystem( Vector3 position )
+    {
+        ParticleSystem currentParticles = ParticleSystemSpawner.current.GetPooledParticleSystem();
+        currentParticles.transform.position = position;
+        currentParticles.Play();
+    }
+}
